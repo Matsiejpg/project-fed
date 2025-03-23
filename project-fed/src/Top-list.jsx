b2662230b392685f78f854c583be6045ee0ac50c
Mats <matsdevegt@gmail.com>
2025-03-23 13:52:36 +0100

    worked further on the top list

--- project-fed/src/Top-list.jsx
+++ project-fed/src/Top-list.jsx
@@ -4,23 +4,21 @@ function TopList() {
   const [coins, setCoins] = useState([]);
 
   useEffect(() => {
-    // Stap 1: Haal de eerste 10 coins op
     fetch("https://api.coinpaprika.com/v1/coins")
       .then((response) => response.json())
       .then((data) => {
         const top10Coins = data.slice(0, 10);
 
-        // Stap 2: Voor elke coin een fetch naar de prijs-data maken
         const pricePromises = top10Coins.map((coin) =>
           fetch(`https://api.coinpaprika.com/v1/tickers/${coin.id}`)
             .then((response) => response.json())
             .then((priceData) => ({
-              ...coin, // Houd de bestaande coin info
-              price: priceData.quotes.USD.price, // Voeg de prijs toe
+              ...coin,
+              price: priceData.quotes.USD.price,
+              marketCap: priceData.quotes.USD.market_cap,
             }))
         );
 
-        // Stap 3: Wacht tot alle prijzen binnen zijn
         Promise.all(pricePromises).then((coinsWithPrices) => {
           setCoins(coinsWithPrices);
         });
@@ -28,22 +26,45 @@ function TopList() {
       .catch((error) => console.error(error));
   }, []);
 
+  const getDecimal = (price) => {
+    if (price >= 1) return 2;
+    if (price >= 0.1) return 4;
+    return 8;
+  };
+
   return (
     <>
       <h1>Top 10 Coins</h1>
       <ul className="coins">
         {coins.map((coin) => (
           <li key={coin.id} className="coin">
+            <p>{coin.rank}</p>
             <img
               src={`https://static.coinpaprika.com/coin/${coin.id}/logo.png`}
               alt={coin.name}
               className="coin-logo"
             />
             <div className="coin-info">
-              <h2>
-                {coin.name} ({coin.symbol})
-              </h2>
-              <p>Prijs: ${coin.price ? coin.price.toFixed(2) : "Laden..."}</p>
+              <h2>{coin.name}</h2>
+              <p>{coin.symbol}</p>
+            </div>
+            <div>
+              <h3>Prijs</h3>
+              <p>
+                $
+                {coin.price
+                  ? coin.price.toFixed(getDecimal(coin.price))
+                  : coin.price}
+              </p>
+            </div>
+            <div>
+              <h3>Market cap</h3>
+              <p>
+                $
+                {coin.marketCap
+                  ? coin.marketCap.toFixed(getDecimal(coin.marketCap))
+                  : coin.marketCap}
+              </p>
             </div>
           </li>
         ))}
